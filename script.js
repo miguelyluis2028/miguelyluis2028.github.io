4f05a00b3ab20f14891eb8b396ad849db7a9cfc0
miguelyluis2028 <302306311+miguelyluis2028@users.noreply.github.com>
2026-07-16 13:04:31 -0400

    Sprint 1 - Portada Oficial 1.0

---
+++ script.js
@@ -0,0 +1,26 @@
+/* ===================================================
+   ETERNA
+   Portada Oficial 1.0
+   FROZEN v1.0
+=================================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const startButton = document.getElementById("startButton");
+
+    startButton.addEventListener("click", () => {
+
+        document.body.classList.add("fade-out");
+
+        console.log("Comenzar ETERNA");
+
+        /*
+            Aquí comenzará la Escena 1.
+
+            En Sprint 2 conectaremos esta acción
+            con el SceneManager.
+        */
+
+    });
+
+});
