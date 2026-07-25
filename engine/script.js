4ae8602dbf2d8dced0fc78bb6e7f1e993bd2abb5
miguelyluis2028 <302306311+miguelyluis2028@users.noreply.github.com>
2026-07-24 22:38:58 -0400

    First real page

--- engine/script.js
+++ engine/script.js
@@ -1,6 +1,21 @@
-const book = document.getElementById("book");
-book.addEventListener("click", () => {
+const sheet = document.querySelector(".sheet");
 
-    book.style.transform = "rotateY(-25deg)";
+let open = false;
+
+sheet.addEventListener("click", () => {
+
+    if (!open){
+
+        sheet.style.transform = "rotateY(-180deg)";
+
+        open = true;
+
+    }else{
+
+        sheet.style.transform = "rotateY(0deg)";
+
+        open = false;
+
+    }
 
 });
