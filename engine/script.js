7756bb6a2d2327b0104bf2635d5027ad6d75b6cf
miguelyluis2028 <302306311+miguelyluis2028@users.noreply.github.com>
2026-07-25 22:39:04 -0400

    jumm

--- engine/script.js
+++ engine/script.js
@@ -1,19 +1,1 @@
 const page = document.querySelector(".page");
-
-let opened = false;
-
-page.addEventListener("click", () => {
-
-    if (opened){
-
-        page.style.transform = "rotateY(0deg)";
-
-    }else{
-
-        page.style.transform = "rotateZ(20deg)";
-
-    }
-
-    opened = !opened;
-
-});
