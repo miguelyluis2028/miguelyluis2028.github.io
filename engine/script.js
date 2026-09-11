ee4e1ef4e5539af500e5d048cebe63ed625283a7
miguelyluis2028 <302306311+miguelyluis2028@users.noreply.github.com>
2026-09-11 18:13:44 -0400

    First page turn interaction

--- engine/script.js
+++ engine/script.js
@@ -1,1 +1,13 @@
 const page = document.querySelector(".page");
+
+let turned = false;
+
+page.addEventListener("click", () => {
+    turned = !turned;
+
+    if (turned) {
+        page.style.transform = "rotateY(-180deg)";
+    } else {
+        page.style.transform = "rotateY(0deg)";
+    }
+});
