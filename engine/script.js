6e79296ccd6c6a2b6f86208c6eba66e41b33ffa6
miguelyluis2028 <302306311+miguelyluis2028@users.noreply.github.com>
2026-09-12 18:46:24 -0400

    Engine: 20 image sequence

--- engine/script.js
+++ engine/script.js
@@ -1,17 +1,46 @@
 const page = document.querySelector(".page");
 
+const images = [];
+
+for (let i = 1; i <= 20; i++) {
+  images.push(`../images/libro/pagina-${i}.png`);
+}
+
+let current = 0;
 let turned = false;
+
+const frontImage = document.querySelector(".front img");
+const backImage = document.querySelector(".back img");
+const leftImage = document.querySelector(".left-page img");
+
+function showSpread(index) {
+  const image = images[index];
+
+  leftImage.src = image;
+  frontImage.src = image;
+
+  if (index < images.length - 1) {
+    backImage.src = images[index + 1];
+  }
+}
+
+showSpread(current);
 
 page.style.transform = "rotateY(0deg)";
 
 document.addEventListener("pointerdown", (event) => {
   event.preventDefault();
 
-  turned = !turned;
+  if (!turned) {
+    if (current < images.length - 1) {
+      current++;
+      showSpread(current);
 
-  if (turned) {
-    page.style.transform = "rotateY(-180deg)";
+      page.style.transform = "rotateY(-180deg)";
+      turned = true;
+    }
   } else {
     page.style.transform = "rotateY(0deg)";
+    turned = false;
   }
 });
